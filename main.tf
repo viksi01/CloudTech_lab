79b2fac38433412b10af8f9e690a797b3f37e4e3
viksi01 <viktoriia.piatnytska.ri.2022@lpnu.ua>
2024-03-19 21:06:19 +0200

    created dynamodb files

---
+++ main.tf
@@ -0,0 +1,10 @@
+module "table_authors" {
+  source = "./modules/dynamodb"
+  table_name = "authors"
+}
+
+module "table_courses" {
+  source = "./modules/dynamodb"
+  table_name = "courses"
+
+}
---
+++ modules/dynamodb/main.tf
@@ -0,0 +1,13 @@
+resource "aws_dynamodb_table" "this" {
+  name           = var.table_name
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  
+}
